66746d33545faa5f546cb804db74233a154ef966
Pierre GIRAUD <pierre.giraud@dalibo.com>
2019-08-26 15:37:12 +0200

    Handle Workers duplicated key in JSON

--- src/services/plan-service.ts
+++ src/services/plan-service.ts
@@ -4,6 +4,7 @@ import { IPlan } from '@/iplan';
 import Node from '@/inode';
 import Worker from '@/iworker';
 import moment from 'moment';
+import clarinet from 'clarinet';
 
 export class PlanService {
 
@@ -200,11 +201,71 @@ export class PlanService {
 
     const useSource: string = sourceLines.slice(firstLineIndex, lastLineIndex + 1).join('\n');
 
-    let planJson = JSON.parse(useSource);
+    let planJson = this.parseJson(useSource);
     if (planJson instanceof Array) {
       planJson = planJson[0];
     }
     return planJson;
+  }
+
+  // Stream parse JSON as it can contain duplicate keys (workers)
+  public parseJson(source: string) {
+    const parser = clarinet.parser();
+    const elements: any[] = [];
+    let root: any = null;
+    // Store the level and duplicated object|array
+    let duplicated: [number, any] | null = null;
+    parser.onvalue = (v: any) => {
+      const current = elements[elements.length - 1];
+      if (_.isArray(current)) {
+        current.push(v);
+      } else {
+        const keys = Object.keys(current);
+        const lastKey = keys[keys.length - 1];
+        current[lastKey] = v;
+      }
+    };
+    parser.onopenobject = (key: any) => {
+      const o: {[key: string]: any} = {};
+      o[key] = null;
+      elements.push(o);
+    };
+    parser.onkey = (key: any) => {
+      const current = elements[elements.length - 1];
+      const keys = Object.keys(current);
+      if (keys.indexOf(key) !== -1) {
+        duplicated = [elements.length - 1, current[key]];
+      } else {
+        current[key] = null;
+      }
+    };
+    parser.onopenarray = () => {
+      elements.push([]);
+    };
+    parser.oncloseobject = parser.onclosearray = () => {
+      const popped = elements.pop();
+
+      if (!elements.length) {
+        root = popped;
+      } else {
+        const current = elements[elements.length - 1];
+
+        if (duplicated && duplicated[0] === elements.length - 1) {
+          _.merge(duplicated[1], popped);
+          duplicated = null;
+        } else {
+          if (_.isArray(current)) {
+            current.push(popped);
+          } else {
+            const keys = Object.keys(current);
+            const lastKey = keys[keys.length - 1];
+            current[lastKey] = popped;
+          }
+        }
+      }
+    };
+    parser.write(source).close();
+    return root;
   }
 
   public fromText(text: string) {
