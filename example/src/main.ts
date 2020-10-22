0ee4bdfeb5958a7d33fcb6fbdd5c2841924abe8b
Jannis R <mail@jannisr.de>
2020-10-22 14:26:10 +0200

    example: expose fn to set plan & query programmatically

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,11 +1,19 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App, { planData } from './App.vue';
 import router from './router';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 Vue.config.productionTip = false;
+
+export function setPlanData (plan, query) {
+  planData[0] = plan;
+  planData[1] = query;
+  router.push({ path: 'plan' });
+};
+
+global.setPlanData = setPlanData;
 
 new Vue({
   router,
