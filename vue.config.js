40e0fa9eba72e7483cc662e521aaeda2589cd1ec
Pierre GIRAUD <pierre.giraud@dalibo.com>
2021-02-05 09:18:28 +0100

    Don't use source map for production

--- vue.config.js
+++ vue.config.js
@@ -2,19 +2,20 @@ const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
 
 module.exports = {
   publicPath: "",
+  productionSourceMap: process.env.NODE_ENV != "production",
   pages: {
     index: {
       entry: "example/src/main.ts",
-      template: "example/index.html"
-    }
+      template: "example/index.html",
+    },
   },
   css: {
-    extract: false
+    extract: false,
   },
   configureWebpack: {
     plugins: [
       // To strip all locales except “en”
-      new MomentLocalesPlugin()
-    ]
-  }
+      new MomentLocalesPlugin(),
+    ],
+  },
 };
