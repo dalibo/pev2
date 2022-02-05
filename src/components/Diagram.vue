905038e588469d727b54e47c396b530ed53bb4c1
Pierre GIRAUD <pierre.giraud@dalibo.com>
2022-02-05 09:45:32 -0500

    Fix wrong character encoding

--- src/components/Diagram.vue
+++ src/components/Diagram.vue
@@ -305,7 +305,7 @@ export default class Diagram extends Vue {
     text += (estimateFactor !== 1) ? ' by <b>' + factor(estimateFactor) + '</b>' : '';
     text += '<br>';
     text += 'Planned: ' + node[NodeProp.PLAN_ROWS_REVISED];
-    text += ' 🡒 Actual: ' + node[NodeProp.ACTUAL_ROWS_REVISED];
+    text += ' → Actual: ' + node[NodeProp.ACTUAL_ROWS_REVISED];
     return text;
   }
 
