98209ccded993fe329716c35e75724a964bfb80c
Pierre GIRAUD <pierre.giraud@dalibo.com>
2022-03-16 08:47:28 +0100

    Different build for app or lib

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,31 @@ import { fileURLToPath, URL } from "url"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/components/index.ts"),
-      name: "pev2",
-      fileName: (format) => `pev2.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // Add external deps here
-        globals: {
-          vue: "Vue",
+const build = process.env.LIB
+  ? {
+      lib: {
+        entry: path.resolve(__dirname, "src/components/index.ts"),
+        name: "pev2",
+        fileName: (format: string) => `pev2.${format}.js`,
+      },
+      rollupOptions: {
+        external: ["vue"],
+        output: {
+          // Provide global variables to use in the UMD build
+          // Add external deps here
+          globals: {
+            vue: "Vue",
+          },
         },
       },
-    },
-  },
+    }
+  : {
+      outDir: "dist-app",
+    }
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: build,
   plugins: [vue()],
   resolve: {
     alias: {
