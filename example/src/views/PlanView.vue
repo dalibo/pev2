17f046e56699e85f2de032608c4589113ce820f9
Pierre GIRAUD <pierre.giraud@dalibo.com>
2022-03-16 10:39:43 +0100

    planData are refs

--- example/src/views/PlanView.vue
+++ example/src/views/PlanView.vue
@@ -10,8 +10,8 @@ const planData = inject("planData")
   <main-layout>
     <plan
       v-if="planData[0]"
-      :plan-source="planData[0]"
-      :plan-query="planData[1]"
+      :plan-source="planData[0].value"
+      :plan-query="planData[1].value"
     />
   </main-layout>
 </template>
