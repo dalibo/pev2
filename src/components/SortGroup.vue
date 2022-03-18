f9da14292940aceea4e6667ad4508871bed1d2b9
Pierre GIRAUD <pierre.giraud@dalibo.com>
2022-03-18 15:58:57 +0100

    Reimplement Plan component

---
+++ src/components/SortGroup.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import { keysToString, kilobytes } from "@/filters"
+import { SortGroup } from "@/interfaces"
+
+interface Props {
+  sortGroup: SortGroup
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <div>
+    Count: {{ sortGroup["Group Count"] }}
+    <br />
+    <table class="table-borderless">
+      <tr>
+        <td class="text-nowrap p-0">
+          Sort Method<template v-if="sortGroup['Sort Methods Used'].length > 1"
+            >s</template
+          >:
+        </td>
+        <td class="p-0 pl-1">
+          {{ keysToString(sortGroup["Sort Methods Used"]) }}
+        </td>
+      </tr>
+    </table>
+    <table class="table-borderless">
+      <tr>
+        <td class="p-0">Memory:</td>
+        <td class="p-0 pl-1">
+          Average:
+          {{
+            kilobytes(sortGroup["Sort Space Memory"]["Average Sort Space Used"])
+          }}
+          <br />
+          Peak:
+          {{
+            kilobytes(sortGroup["Sort Space Memory"]["Peak Sort Space Used"])
+          }}
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
