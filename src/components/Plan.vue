b15310d921bb3ab0044d6a3553e98d4ddd93eb61
Pierre GIRAUD <pierre.giraud@dalibo.com>
2022-04-19 17:46:21 +0200

    Make tippy style work
    Import tippy css

--- src/components/Plan.vue
+++ src/components/Plan.vue
@@ -38,6 +38,8 @@ import {
 } from "@/enums"
 import { duration, durationClass, json_, pgsql_ } from "@/filters"
 
+import "tippy.js/dist/tippy.css"
+
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fas } from "@fortawesome/free-solid-svg-icons"
 import { far } from "@fortawesome/free-regular-svg-icons"
