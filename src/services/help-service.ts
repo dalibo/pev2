6577440f989f062872b0849b844e5b524dfd3ead
meo <meob@users.noreply.github.com>
2023-02-17 15:09:34 +0100

    Update help-service.ts
    Adding GATHER and GATHER MERGE node description

--- src/services/help-service.ts
+++ src/services/help-service.ts
@@ -46,6 +46,8 @@ export const NODE_DESCRIPTIONS: INodeDescription = {
   "CTE SCAN": `performs a sequential scan of <strong>Common Table Expression (CTE) query</strong> results. Note that
     results of a CTE are materialized (calculated and temporarily stored).`,
   MEMOIZE: `is used to cache the results of the inner side of a nested loop. It avoids executing underlying nodes when the results for the current parameters are already in the cache.`,
+  GATHER: `is used perform a parallel execution. The portion of the plan below it will run in parallel.`,
+  "GATHER MERGE": `is used perform a parallel execution and merging the results. The portion of the plan below it will run in parallel.`,
 }
 
 interface IHelpMessage {
