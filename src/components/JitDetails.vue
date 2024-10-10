6400f12e8dbd22e725a01e352be5b99cbe45c642
Pierre GIRAUD <pierre.giraud@dalibo.com>
2024-10-10 15:30:29 +0200

    Show details for JIT
    In PlanStats as well as in the workers tab in PlanNodeDetail.
    
    Plan in 05-parallel-jit.ts is a good example.

---
+++ src/components/JitDetails.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import type { JIT } from "@/interfaces"
+interface Props {
+  jit: JIT
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <table class="table table-sm table-borderless">
+    <tbody>
+      <tr>
+        <th>Functions</th>
+        <td class="text-end">{{ props.jit.Functions }}</td>
+      </tr>
+      <tr>
+        <th colspan="2">Options</th>
+      </tr>
+      <tr v-for="(option, index) in props.jit.Options" :key="index">
+        <td>&nbsp;&nbsp;{{ index }}</td>
+        <td class="text-end">
+          {{ option ? "✓" : "✗" }} <small>({{ option }})</small>
+        </td>
+      </tr>
+      <tr>
+        <th colspan="2">Timing</th>
+      </tr>
+      <tr v-for="(timing, index) in props.jit.Timing" :key="index">
+        <td>&nbsp;&nbsp;{{ index }}</td>
+        <td class="text-end">{{ timing }} ms</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
