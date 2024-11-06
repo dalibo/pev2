227c91eb81aa330c97d174b66e1860c516aecb98
zihe-git <ziheliu85@gmail.com>
2024-11-06 13:59:16 +0800

    feat: Parse Memory used and Optimizer

--- src/services/plan-service.ts
+++ src/services/plan-service.ts
@@ -484,7 +484,7 @@ export class PlanService {
         out[out.length - 1] += line
       } else if (
         line.match(
-          /^(?:Total\s+runtime|Planning\s+time|Execution\s+time|Time|Filter|Output|JIT)/i
+          /^(?:Total\s+runtime|Planning\s+time|Memory\s+used|Optimizer|Execution\s+time|Time|Filter|Output|JIT)/i
         )
       ) {
         out.push(line)
