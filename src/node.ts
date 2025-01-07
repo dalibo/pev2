fcf756578885ad176d763bb1ff686ce396614b57
Marion Giusti <marion.giusti@dalibo.com>
2025-01-07 09:29:15 +0100

    Take index recheck into account
    The "rows filtered by index recheck" and "recheck cond" are taken into
    account and now appear in the General tab in the node detail.
    
    rowsRemovedProp doesn't need to be a "computed". It's value will not
    change.

--- src/node.ts
+++ src/node.ts
@@ -108,22 +108,20 @@ export default function useNode(
   }
 
   type NodePropStrings = keyof typeof NodeProp
-  const rowsRemovedProp = computed((): NodePropStrings => {
-    const nodeKey = Object.keys(node).find(
-      (key) =>
-        key === NodeProp.ROWS_REMOVED_BY_FILTER_REVISED ||
-        key === NodeProp.ROWS_REMOVED_BY_JOIN_FILTER_REVISED
-    )
-    return Object.keys(NodeProp).find(
-      (prop) => NodeProp[prop as NodePropStrings] === nodeKey
-    ) as NodePropStrings
-  })
+  const nodeKey = Object.keys(node).find(
+    (key) =>
+      key === NodeProp.ROWS_REMOVED_BY_FILTER_REVISED ||
+      key === NodeProp.ROWS_REMOVED_BY_JOIN_FILTER_REVISED ||
+      key === NodeProp.ROWS_REMOVED_BY_INDEX_RECHECK_REVISED
+  )
+  const rowsRemovedProp: NodePropStrings =  Object.keys(NodeProp).find(
+    (prop) => NodeProp[prop as NodePropStrings] === nodeKey
+  ) as NodePropStrings
 
   function calculateRowsRemoved() {
-    if (rowsRemovedProp.value) {
-      type NodePropStrings = keyof typeof NodeProp
+    if (rowsRemovedProp) {
       const removed = node[
-        NodeProp[rowsRemovedProp.value as NodePropStrings]
+        NodeProp[rowsRemovedProp]
       ] as number
       rowsRemoved.value = removed
       const actual = node[NodeProp.ACTUAL_ROWS_REVISED]
@@ -231,6 +229,12 @@ export default function useNode(
     return `Filter used:<br><pre class="mb-0" style="white-space: pre-wrap;"><code>${
       node[NodeProp.FILTER]
     }</code></pre>`
+  })
+
+  const indexRecheckTooltip = computed((): string => {
+    return `Recheck condition:<br><pre class="mb-0" style="white-space: pre-wrap;"><code>${
+      node[NodeProp.RECHECK_COND]
+    }</code></pre><br>Not enough memory, try increasing <em>work_mem</em>.`
   })
 
   const isNeverExecuted = computed((): boolean => {
@@ -547,6 +551,7 @@ export default function useNode(
     heapFetchesClass,
     heapFetchesTooltip,
     highlightValue,
+    indexRecheckTooltip,
     ioTooltip,
     isNeverExecuted,
     isParallelAware,
