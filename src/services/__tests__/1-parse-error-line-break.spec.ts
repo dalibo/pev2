e4a1b1b1128627b4b8b77490834910edce1aee29
Pierre GIRAUD <pierre.giraud@dalibo.com>
2025-06-10 09:53:25 +0200

    Remove line break for more info lines (not only Output)
    Until now, only lines following "Output:" lines were possibly considered
    as continuation or the list of items for Ouput. However this can also
    happen for lines like "Sort key:". We now rely on the presence of
    a trailing coma.

--- src/services/__tests__/1-parse-error-line-break.spec.ts
+++ src/services/__tests__/1-parse-error-line-break.spec.ts
@@ -26,4 +26,22 @@ Execution Time: 0.170 ms`
     const plan: IPlan = planService.createPlan("", r, "")
     expect(plan.content["Planning Time"]).toBe(1.11)
   })
+
+  test("Can`t parse plan with line break", () => {
+    const planService = new PlanService()
+    // tslint:disable:max-line-length
+    const source = `
+ Some Node Type  (cost=1305.69..1734.14 rows=1 width=55)
+   CTE cte1
+     ->  Sort  (cost=8.57..8.57 rows=2 width=583)
+           Sort Key: something,
+ blah
+   CTE cte2
+     ->  CTE Scan on enabledseasons  (cost=0.00..0.04 rows=1 width=8)
+`
+    planService.fromText(source)
+    expect(() => {
+      planService.fromText(source)
+    }).not.toThrow()
+  })
 })
