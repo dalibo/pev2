1d866c1f8c8bd96f2d52f3890b82f21e010c5dd0
Pierre GIRAUD <pierre.giraud@dalibo.com>
2025-11-07 16:05:02 +0100

    Paginate saved plans and allow selection for bulk delete

--- example/src/views/HomeView.vue
+++ example/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { computed, inject, ref, onMounted } from "vue"
+import { computed, inject, ref, onMounted, watch } from "vue"
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 
@@ -18,31 +18,24 @@ const queryInput = ref<string>("")
 const queryName = ref<string>("")
 const draggingPlan = ref<boolean>(false)
 const draggingQuery = ref<boolean>(false)
-const savedPlans = ref<Plan[]>()
-const pageSize = 9
+const savedPlans = ref<Plan[]>([])
+const pageSize = 10
 const maxVisiblePages = 5
 const currentPage = ref<number>(1)
 const totalPages = computed(() => {
-  if (!savedPlans.value) {
-    return 0
-  }
   return Math.ceil(savedPlans.value.length / pageSize)
 })
 const hovered = ref(null)
+const selectionMode = ref(false)
+const selection = ref<Plan[]>([])
 
 const paginatedPlans = computed(() => {
-  if (!savedPlans.value) {
-    return []
-  }
   const start = (currentPage.value - 1) * pageSize
   const end = start + pageSize
   return savedPlans.value.slice(start, end)
 })
 
 const visiblePages = computed(() => {
-  if (!savedPlans.value) {
-    return []
-  }
   const total = totalPages.value
   const half = Math.floor(maxVisiblePages / 2)
   let start = currentPage.value - half
@@ -111,12 +104,52 @@ function loadPlan(plan?: Plan) {
   queryInput.value = plan[2]
 }
 
+function openOrSelectPlan(plan: Plan) {
+  if (!selectionMode.value) {
+    openPlan(plan)
+  } else {
+    togglePlanSelection(plan)
+  }
+}
+
 function openPlan(plan: Plan) {
   setPlanData(plan[0], plan[1], plan[2])
 }
 
+function isSelected(id: integer) {
+  return selection.value.includes(id)
+}
+
+function togglePlanSelection(plan) {
+  const index = selection.value.indexOf(plan.id)
+  if (index === -1) {
+    selection.value.push(plan.id)
+  } else {
+    selection.value.splice(index, 1)
+  }
+}
+
 function editPlan(plan: Plan) {
   loadPlan(plan)
+}
+
+function plansFromSelection() {
+  return selection.value.length > 0
+    ? savedPlans.value.filter((p) => selection.value.includes(p.id))
+    : savedPlans.value
+}
+
+function deletePlans() {
+  if (confirm("Are you sure you want to delete plans?")) {
+    const plans = plansFromSelection()
+    plans.forEach(async (plan) => {
+      await idb.deletePlan(plan)
+    })
+    loadPlans()
+    selectionMode.value = false
+  }
+  // reset page
+  currentPage.value = 1
 }
 
 async function deletePlan(plan: Plan) {
@@ -159,6 +192,13 @@ function prevPage() {
 function goToPage(page) {
   currentPage.value = page
 }
+
+watch(
+  () => selectionMode.value,
+  () => {
+    selection.value = []
+  },
+)
 </script>
 
 <template>
@@ -173,49 +213,54 @@ function goToPage(page) {
         <a href="https://explain.dalibo.com">explain.dalibo.com</a> instead if
         you want to save or share your plans.
       </div>
-      <div class="row mb-3">
-        <div class="col d-flex">
-          <div class="text-secondary">
-            For best results, use
-            <code>
-              EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON)
-            </code>
-            <br />
-            <em>psql</em> users can export the plan to a file using
-            <code>psql -XqAt -f explain.sql > analyze.json</code>
-          </div>
-          <div class="dropdown ms-auto">
-            <button
-              class="btn btn-secondary dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton"
-              data-bs-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Sample Plans
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a
-                v-for="(sample, index) in samples"
-                :key="index"
-                class="dropdown-item"
-                v-on:click.prevent="loadPlan(sample)"
-                href=""
-              >
-                {{ sample[0] }}
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
       <div class="row">
         <div class="col-sm-7">
+          <div class="row mb-3">
+            <div class="col d-flex">
+              <div class="text-secondary">
+                For best results, use
+                <code>
+                  EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON)
+                </code>
+                <br />
+                <em>psql</em> users can export the plan to a file using
+                <code>psql -XqAt -f explain.sql > analyze.json</code>
+              </div>
+            </div>
+          </div>
           <form v-on:submit.prevent="submitPlan">
             <div class="mb-3">
-              <label for="planInput" class="form-label">
-                Plan <span class="small text-secondary">(text or JSON)</span>
-              </label>
+              <div class="d-flex align-items-center mb-2">
+                <label for="planInput" class="form-label">
+                  Plan <span class="small text-secondary">(text or JSON)</span>
+                </label>
+                <div class="dropdown ms-auto">
+                  <button
+                    class="btn btn-secondary dropdown-toggle btn-sm"
+                    type="button"
+                    id="dropdownMenuButton"
+                    data-bs-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Sample Plans
+                  </button>
+                  <div
+                    class="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton"
+                  >
+                    <a
+                      v-for="(sample, index) in samples"
+                      :key="index"
+                      class="dropdown-item"
+                      v-on:click.prevent="loadPlan(sample)"
+                      href=""
+                    >
+                      {{ sample[0] }}
+                    </a>
+                  </div>
+                </div>
+              </div>
               <textarea
                 :class="['form-control', draggingPlan ? 'dropzone-over' : '']"
                 id="planInput"
@@ -259,31 +304,124 @@ function goToPage(page) {
             <button type="submit" class="btn btn-primary">Submit</button>
           </form>
         </div>
-        <div class="col-sm-5 mb-4 mt-4 mt-md-0">
-          <div class="mb-2">Saved Plans</div>
+        <div class="col-sm-5">
+          <div class="d-flex flex-row mb-2 align-items-center">
+            <div>Saved Plans</div>
+            <div class="d-flex align-items-center mb-2 ms-auto">
+              <nav>
+                <ul class="pagination pagination-sm m-0">
+                  <li
+                    class="page-item"
+                    :class="{ disabled: currentPage === 1 }"
+                  >
+                    <a
+                      class="page-link"
+                      href="#"
+                      @click="prevPage"
+                      aria-label="Previous"
+                    >
+                      <span aria-hidden="true">&laquo;</span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                  </li>
+                  <li
+                    class="page-item"
+                    v-if="visiblePages[0] > 1"
+                    @click="goToPage(1)"
+                  >
+                    <a class="page-link" href="#">1</a>
+                  </li>
+                  <li class="page-item" v-if="visiblePages[0] > 2">
+                    <a class="page-link"> … </a>
+                  </li>
+                  <li
+                    class="page-item"
+                    v-for="page in visiblePages"
+                    :key="page"
+                    @click="goToPage(page)"
+                    :class="{ active: page === currentPage }"
+                  >
+                    <a class="page-link" href="#">{{ page }}</a>
+                  </li>
+                  <li
+                    class="page-item"
+                    v-if="
+                      visiblePages[visiblePages.length - 1] < totalPages - 1
+                    "
+                  >
+                    <a class="page-link"> … </a>
+                  </li>
+                  <li
+                    class="page-item"
+                    v-if="visiblePages[visiblePages.length - 1] < totalPages"
+                    @click="goToPage(totalPages)"
+                  >
+                    <a class="page-link" href="#">{{ totalPages }}</a>
+                  </li>
+                  <li
+                    class="page-item"
+                    :class="{ disabled: currentPage === totalPages }"
+                  >
+                    <a
+                      class="page-link"
+                      href="#"
+                      @click="nextPage"
+                      aria-labal="Next"
+                    >
+                      <span aria-hidden="true">&raquo;</span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+              <button
+                class="btn btn-sm ms-2 btn-light"
+                :class="{ active: selectionMode }"
+                @click="selectionMode = !selectionMode"
+              >
+                {{ selectionMode ? "OK" : "Select" }}
+              </button>
+            </div>
+          </div>
           <div class="list-group" v-cloak>
             <a
               class="list-group-item list-group-item-action px-2 py-1 flex-column"
+              :class="{ active: isSelected(plan.id) }"
               v-for="(plan, index) in paginatedPlans"
               :key="plan.id"
               href="#"
-              @click.prevent="openPlan(plan)"
+              @click.prevent="openOrSelectPlan(plan)"
               @mouseenter="hovered = index"
               @mouseleave="hovered = null"
             >
               <div class="d-flex w-100 align-items-center">
+                <input
+                  class="form-check-input me-3"
+                  type="checkbox"
+                  v-if="selectionMode"
+                  :value="plan.id"
+                  v-model="selection"
+                  @click.stop
+                />
                 <div>
                   <p class="mb-0">
                     {{ plan[0] }}
                   </p>
-                  <small class="text-secondary">
+                  <small
+                    :class="{
+                      'text-secondary': !isSelected(plan.id),
+                    }"
+                  >
                     created
                     <span :title="plan[3]?.toString()">
                       {{ time_ago(plan[3]) }}
                     </span>
                   </small>
                 </div>
-                <div class="ms-auto text-nowrap" v-if="hovered === index">
+                <div
+                  class="ms-auto text-nowrap"
+                  v-if="hovered === index && !selectionMode"
+                >
                   <button
                     class="btn btn-sm btn-outline-secondary py-0 ms-1"
                     title="Remove plan from list"
@@ -302,67 +440,13 @@ function goToPage(page) {
               </div>
             </a>
           </div>
-          <nav class="mt-2">
-            <ul class="pagination pagination-sm justify-content-end">
-              <li class="page-item" :class="{ disabled: currentPage === 1 }">
-                <a
-                  class="page-link"
-                  href="#"
-                  @click="prevPage"
-                  aria-label="Previous"
-                >
-                  <span aria-hidden="true">&laquo;</span>
-                  <span class="sr-only">Previous</span>
-                </a>
-              </li>
-              <li
-                class="page-item"
-                v-if="visiblePages[0] > 1"
-                @click="goToPage(1)"
-              >
-                <a class="page-link" href="#">1</a>
-              </li>
-              <li class="page-item" v-if="visiblePages[0] > 2">
-                <a class="page-link"> … </a>
-              </li>
-              <li
-                class="page-item"
-                v-for="page in visiblePages"
-                :key="page"
-                @click="goToPage(page)"
-                :class="{ active: page === currentPage }"
-              >
-                <a class="page-link" href="#">{{ page }}</a>
-              </li>
-              <li
-                class="page-item"
-                v-if="visiblePages[visiblePages.length - 1] < totalPages - 1"
-              >
-                <a class="page-link"> … </a>
-              </li>
-              <li
-                class="page-item"
-                v-if="visiblePages[visiblePages.length - 1] < totalPages"
-                @click="goToPage(totalPages)"
-              >
-                <a class="page-link" href="#">{{ totalPages }}</a>
-              </li>
-              <li
-                class="page-item"
-                :class="{ disabled: currentPage === totalPages }"
-              >
-                <a
-                  class="page-link"
-                  href="#"
-                  @click="nextPage"
-                  aria-labal="Next"
-                >
-                  <span aria-hidden="true">&raquo;</span>
-                  <span class="sr-only">Next</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
+          <div v-if="selectionMode" class="mt-2 d-flex">
+            <button class="btn btn-sm ms-auto btn-danger" @click="deletePlans">
+              <FontAwesomeIcon :icon="faTrash" class="me-2"></FontAwesomeIcon>
+              Delete<template v-if="selection.length < 1"> all</template
+              ><template v-else> ({{ selection.length }})</template>
+            </button>
+          </div>
           <p
             class="text-secondary text-center"
             v-if="!savedPlans?.length"
