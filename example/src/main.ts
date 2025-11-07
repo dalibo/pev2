dalibo/pev2

commit 06dcea951248163fb8974a05eb96577e45059c07

Use component instead of directive for vue-tippy
For this, we use the <tippy> component with a content slot.
Hopefully, we'll get rid of many functions in the node composite
(useNode).

This will make the integration of content with other components easier.

The rootEl ref in DiagramRow is moved so that scroll into view continues
to work.

For the sake of readability, the light theme is now used for VueTippy.

example/src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from "vue"
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.css"
+import VueTippy from "vue-tippy"
 
 import App from "./App.vue"
 
-createApp(App).mount("#app")
+createApp(App).use(VueTippy, { defaultProps: {theme: "light"}}).mount("#app")
 
 declare global {
   interface Window {

src/components/DiagramRow.vue
@@ -11,9 +11,10 @@ import {
   ViewOptionsKey,
 } from "@/symbols"
 import type { IPlan, Node, ViewOptions } from "@/interfaces"
-import { HelpService } from "@/services/help-service"
 import { EstimateDirection, BufferLocation, NodeProp, Metric } from "../enums"
 import LevelDivider from "@/components/LevelDivider.vue"
+import TimeTooltip from "@/components/tooltip/TimeTooltip.vue"
+import IoTooltip from "@/components/tooltip/IoTooltip.vue"
 import useNode from "@/node"
 
 interface Props {
@@ -41,57 +42,15 @@ if (!selectNode) {
 }
 const highlightedNodeId = inject(HighlightedNodeIdKey)
 
-const helpService = new HelpService()
-const getHelpMessage = helpService.getHelpMessage
-
 const _viewOptions = inject(ViewOptionsKey) as ViewOptions
 const {
   buffersByLocationTooltip,
   costTooltip,
   estimateFactorPercent,
   estimateFactorTooltip,
-  ioTooltip,
   nodeName,
   rowsTooltip,
-  timeTooltip,
 } = useNode(plan, node, _viewOptions)
-
-function getTooltipContent(node: Node): string {
-  let content = ""
-  switch (diagramViewOptions.metric) {
-    case Metric.time:
-      content += timeTooltip.value
-      break
-    case Metric.rows:
-      content += rowsTooltip.value
-      break
-    case Metric.estimate_factor:
-      content += estimateFactorTooltip.value
-      break
-    case Metric.cost:
-      content += costTooltip.value
-      break
-    case Metric.buffers:
-      content += buffersByLocationTooltip.value(
-        diagramViewOptions.buffersMetric,
-      )
-      break
-    case Metric.io:
-      content += ioTooltip.value
-
-      if (
-        node[NodeProp.WORKERS_PLANNED] ||
-        node[NodeProp.WORKERS_PLANNED_BY_GATHER]
-      ) {
-        content += `<br><small>${getHelpMessage("io timings parallel")}</small>`
-      }
-      break
-  }
-  if (node[NodeProp.CTE_NAME]) {
-    content += "<br><em>CTE " + node[NodeProp.CTE_NAME] + "</em>"
-  }
-  return content
-}
 
 const scrollTo = inject<(el: Element) => null>("scrollTo")
 
@@ -106,19 +65,47 @@ watch(
 </script>
 
 <template>
-  <tr
+  <tippy
     class="no-focus-outline node"
     :class="{
       selected: node.nodeId === selectedNodeId,
       highlight: node.nodeId === highlightedNodeId,
     }"
-    :data-tippy-content="getTooltipContent(node)"
+    tag="tr"
     @mouseenter="highlightedNodeId = node.nodeId"
     @mouseleave="highlightedNodeId = undefined"
     @click.prevent="selectNode(node.nodeId, true)"
-    ref="rootEl"
   >
-    <td class="node-index">
+    <template #content>
+      <template v-if="node[NodeProp.CTE_NAME]">
+        <div>
+          <em>CTE {{ node[NodeProp.CTE_NAME] }} </em>
+        </div>
+      </template>
+      <time-tooltip
+        :node="node"
+        v-if="diagramViewOptions.metric == Metric.time"
+      />
+      <io-tooltip
+        :node="node"
+        v-else-if="diagramViewOptions.metric == Metric.io"
+      />
+      <template v-else-if="diagramViewOptions.metric == Metric.rows">
+        <div v-html="rowsTooltip"></div>
+      </template>
+      <template v-else-if="diagramViewOptions.metric == Metric.estimate_factor">
+        <div v-html="estimateFactorTooltip"></div>
+      </template>
+      <template v-else-if="diagramViewOptions.metric == Metric.cost">
+        <div v-html="costTooltip"></div>
+      </template>
+      <template v-else-if="diagramViewOptions.metric == Metric.buffers">
+        <div
+          v-html="buffersByLocationTooltip(diagramViewOptions.buffersMetric)"
+        ></div
+      ></template>
+    </template>
+    <td class="node-index" ref="rootEl">
       <span class="fw-normal small">#{{ node.nodeId }} </span>
     </td>
     <td class="node-type pe-2">
@@ -542,5 +529,5 @@ watch(
         ></div>
       </div>
     </td>
-  </tr>
+  </tippy>
 </template>

src/components/GridRow.vue
@@ -19,6 +19,8 @@ import GridProgressBar from "@/components/GridProgressBar.vue"
 import WorkersDetail from "@/components/WorkersDetail.vue"
 import MiscDetail from "@/components/MiscDetail.vue"
 import SeverityBullet from "@/components/SeverityBullet.vue"
+import IoTooltip from "@/components/tooltip/IoTooltip.vue"
+import TimeTooltip from "@/components/tooltip/TimeTooltip.vue"
 import useNode from "@/node"
 import { directive as vTippy } from "vue-tippy"
 import { HelpService } from "@/services/help-service"
@@ -55,7 +57,6 @@ const {
   formattedProp,
   heapFetchesClass,
   heapFetchesTooltip,
-  ioTooltip,
   localDirtiedPercent,
   localHitPercent,
   localReadPercent,
@@ -74,7 +75,6 @@ const {
   sharedWrittenPercent,
   tempReadPercent,
   tempWrittenPercent,
-  timeTooltip,
   tilde,
 } = useNode(plan, node, viewOptions)
 const showDetails = ref<boolean>(false)
@@ -87,10 +87,14 @@ const showDetails = ref<boolean>(false)
         <span class="font-weight-normal small">#{{ node.nodeId }} </span>
       </a>
     </td>
-    <td
+    <tippy
       class="text-end grid-progress-cell text-nowrap"
+      tag="td"
       v-if="columns.includes('time')"
     >
+      <template #content>
+        <time-tooltip :node="node" />
+      </template>
       <GridProgressBar
         :percentage="
           (node[NodeProp.EXCLUSIVE_DURATION] /
@@ -107,10 +111,7 @@ const showDetails = ref<boolean>(false)
         "
       ></GridProgressBar>
       <!-- time -->
-      <div
-        class="position-relative d-flex"
-        v-tippy="{ content: timeTooltip, allowHTML: true }"
-      >
+      <div class="position-relative d-flex">
         <severity-bullet
           :severity="durationClass"
           v-if="durationClass"
@@ -126,12 +127,15 @@ const showDetails = ref<boolean>(false)
           {{ executionTimePercent }}%
         </template>
       </div>
-    </td>
-    <td
+    </tippy>
+    <tippy
       class="text-end grid-progress-cell text-nowrap"
+      tag="td"
       v-if="columns.includes('ioread')"
-      v-tippy="{ content: ioTooltip, allowHTML: true }"
     >
+      <template #content>
+        <io-tooltip :node="node" />
+      </template>
       <template v-if="node[NodeProp.IO_READ_TIME]">
         <GridProgressBar
           :percentage="
@@ -148,12 +152,15 @@ const showDetails = ref<boolean>(false)
           {{ transferRate(node[NodeProp.AVERAGE_IO_READ_SPEED]) }}
         </div>
       </template>
-    </td>
-    <td
+    </tippy>
+    <tippy
       class="text-end grid-progress-cell text-nowrap"
+      tag="td"
       v-if="columns.includes('iowrite')"
-      v-tippy="{ content: ioTooltip, allowHTML: true }"
     >
+      <template #content>
+        <io-tooltip :node="node" />
+      </template>
       <template v-if="node[NodeProp.IO_WRITE_TIME]">
         <GridProgressBar
           :percentage="
@@ -172,7 +179,7 @@ const showDetails = ref<boolean>(false)
           {{ transferRate(node[NodeProp.AVERAGE_IO_WRITE_SPEED]) }}
         </div>
       </template>
-    </td>
+    </tippy>
     <td
       class="text-end grid-progress-cell text-nowrap"
       v-if="columns.includes('rows')"

src/components/Diagram.vue
@@ -3,9 +3,7 @@ import _ from "lodash"
 import {
   computed,
   inject,
-  nextTick,
   onBeforeMount,
-  onMounted,
   provide,
   reactive,
   ref,
@@ -22,8 +20,6 @@ import DiagramRow from "@/components/DiagramRow.vue"
 import LevelDivider from "@/components/LevelDivider.vue"
 
 import { directive as vTippy } from "vue-tippy"
-import tippy, { createSingleton } from "tippy.js"
-import type { CreateSingletonInstance, Instance } from "tippy.js"
 
 const helpService = new HelpService()
 const getHelpMessage = helpService.getHelpMessage
@@ -42,8 +38,6 @@ const highlightedNodeId = inject(HighlightedNodeIdKey)
 
 // The main plan + init plans (all flatten)
 const plans: Row[][] = [[]]
-let tippyInstances: Instance[] = []
-let tippySingleton!: CreateSingletonInstance
 
 const viewOptions = reactive({
   metric: Metric.time,
@@ -71,29 +65,10 @@ onBeforeMount((): void => {
   }
 })
 
-onMounted((): void => {
-  loadTooltips()
-})
-
 watch(viewOptions, onViewOptionsChanged)
 
 function onViewOptionsChanged() {
   localStorage.setItem("diagramViewOptions", JSON.stringify(viewOptions))
-  nextTick(loadTooltips)
-}
-
-function loadTooltips(): void {
-  if (tippySingleton) {
-    tippySingleton.destroy()
-  }
-  _.each(tippyInstances, (instance) => {
-    instance.destroy()
-  })
-  tippyInstances = tippy(".diagram tr.node")
-  tippySingleton = createSingleton(tippyInstances, {
-    delay: 100,
-    allowHTML: true,
-  })
 }
 
 function flatten(

src/components/Plan.vue
@@ -41,6 +41,7 @@ import { HighlightType, NodeProp } from "@/enums"
 import { json_, pgsql_ } from "@/filters"
 
 import "tippy.js/dist/tippy.css"
+import "tippy.js/themes/light.css"
 import * as d3 from "d3"
 import {
   flextree,

src/components/tooltip/IoTooltip.vue
@@ -1,0 +1,46 @@
+<script lang="ts" setup>
+import { ref } from "vue"
+import type { Node } from "@/interfaces"
+import { NodeProp } from "@/enums"
+import { duration, transferRate } from "@/filters"
+import { HelpService } from "@/services/help-service"
+interface Props {
+  node: Node
+}
+const props = defineProps<Props>()
+const read = ref(props.node[NodeProp.EXCLUSIVE_IO_READ_TIME])
+const averageRead = ref(props.node[NodeProp.AVERAGE_IO_READ_SPEED])
+const write = ref(props.node[NodeProp.EXCLUSIVE_IO_WRITE_TIME])
+const averageWrite = ref(props.node[NodeProp.AVERAGE_IO_WRITE_SPEED])
+const helpService = new HelpService()
+const getHelpMessage = helpService.getHelpMessage
+</script>
+
+<template>
+  IO:
+  <table class="table table-sm table-borderless mb-0">
+    <tbody>
+      <tr v-if="read">
+        <td>Read:</td>
+        <td class="text-end">
+          {{ duration(read) }}
+          <br /><small>~ {{ transferRate(averageRead) }}</small>
+        </td>
+      </tr>
+      <tr v-if="write">
+        <td>Write:</td>
+        <td class="text-end">
+          {{ duration(write) }}
+          <br /><small>~ {{ transferRate(averageWrite) }}</small>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <template
+    v-if="
+      node[NodeProp.WORKERS_PLANNED] || node[NodeProp.WORKERS_PLANNED_BY_GATHER]
+    "
+  >
+    <small>{{ getHelpMessage("io timings parallel") }}</small>
+  </template>
+</template>

src/components/tooltip/TimeTooltip.vue
@@ -1,0 +1,15 @@
+<script lang="ts" setup>
+import type { Node } from "@/interfaces"
+import { NodeProp } from "@/enums"
+import { duration } from "@/filters"
+interface Props {
+  node: Node
+}
+defineProps<Props>()
+</script>
+
+<template>
+  Duration:
+  <br />Exclusive: {{ duration(node[NodeProp.EXCLUSIVE_DURATION]) }}, Total:
+  {{ duration(node[NodeProp.ACTUAL_TOTAL_TIME]) }}
+</template>

src/node.ts
@@ -9,15 +9,7 @@ import {
   EstimateDirection,
   HighlightType,
 } from "@/enums"
-import {
-  blocks,
-  cost,
-  duration,
-  factor,
-  formatNodeProp,
-  rows,
-  transferRate,
-} from "@/filters"
+import { blocks, cost, duration, factor, formatNodeProp, rows } from "@/filters"
 import { numberToColorHsl } from "@/services/color-service"
 
 export default function useNode(
@@ -370,15 +362,6 @@ export default function useNode(
     )
   })
 
-  const timeTooltip = computed((): string => {
-    return [
-      "Duration: <br>Exclusive: ",
-      duration(node[NodeProp.EXCLUSIVE_DURATION]),
-      ", Total: ",
-      duration(node[NodeProp.ACTUAL_TOTAL_TIME]),
-    ].join("")
-  })
-
   const rowsTooltip = computed((): string => {
     return ["Rows: ", rows(node[NodeProp.ACTUAL_ROWS_REVISED] as number)].join(
       "",
@@ -461,8 +444,7 @@ export default function useNode(
             written = node[NodeProp.EXCLUSIVE_LOCAL_WRITTEN_BLOCKS]
             break
         }
-        text +=
-          '<table class="table table-dark table-sm table-borderless mb-0">'
+        text += '<table class="table table-sm table-borderless mb-0">'
         text += hit
           ? '<tr><td>Hit:</td><td class="text-end">' +
             blocks(hit, true) +
@@ -505,38 +487,12 @@ export default function useNode(
   )
 
   const buffersByMetricTooltip = computed(() => (metric: NodeProp): string => {
-    let text = '<table class="table table-dark table-sm table-borderless mb-0">'
+    let text = '<table class="table table-sm table-borderless mb-0">'
     text += `<tr><td>${metric}:</td><td class="text-end">`
     if (node[metric]) {
       text += `${blocks(node[metric] as number, true)}</td></tr>`
     }
     return text
-  })
-
-  const ioTooltip = computed((): string => {
-    let text = ""
-    const read = node[NodeProp.EXCLUSIVE_IO_READ_TIME]
-    const averageRead = node[NodeProp.AVERAGE_IO_READ_SPEED]
-    const write = node[NodeProp.EXCLUSIVE_IO_WRITE_TIME]
-    const averageWrite = node[NodeProp.AVERAGE_IO_WRITE_SPEED]
-    text += '<table class="table table-dark table-sm table-borderless mb-0">'
-    text += read
-      ? '<tr><td>Read:</td><td class="text-end">' +
-        duration(read) +
-        "<br><small>~" +
-        transferRate(averageRead) +
-        "</small>" +
-        "</td></tr>"
-      : ""
-    text += write
-      ? '<tr><td>Write:</td><td class="text-end">' +
-        duration(write) +
-        "<br><small>~" +
-        transferRate(averageWrite) +
-        "</small>" +
-        "</td></tr>"
-      : ""
-    return "IO " + text
   })
 
   const heapFetchesTooltip = computed((): string => {
@@ -568,7 +524,6 @@ export default function useNode(
     heapFetchesClass,
     heapFetchesTooltip,
     highlightValue,
-    ioTooltip,
     isNeverExecuted,
     isParallelAware,
     localDirtiedPercent,
@@ -592,7 +547,6 @@ export default function useNode(
     tempReadPercent,
     tempWrittenPercent,
     tilde,
-    timeTooltip,
     workersLaunchedCount,
     workersPlannedCount,
     workersPlannedCountReversed,

commit 3ac0e11afc4a0610f9a25705bc78ae5bcd2b2a60

Don't install tippy component globally
Instead install it locally explicitly in components that need it.

This prevents the need to install it in standalone pages.

Example provided in README was broken since commit 06dcea9512.

Fixes #829

example/src/main.ts
@@ -1,11 +1,10 @@
 import { createApp } from "vue"
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.css"
-import VueTippy from "vue-tippy"
 
 import App from "./App.vue"
 
-createApp(App).use(VueTippy, { defaultProps: {theme: "light"}}).mount("#app")
+createApp(App).mount("#app")
 
 declare global {
   interface Window {

src/components/DiagramRow.vue
@@ -17,6 +17,8 @@ import TimeTooltip from "@/components/tooltip/TimeTooltip.vue"
 import IoTooltip from "@/components/tooltip/IoTooltip.vue"
 import useNode from "@/node"
 
+import { Tippy } from "vue-tippy"
+
 interface Props {
   node: Node
   level: number

src/components/GridRow.vue
@@ -22,7 +22,7 @@ import SeverityBullet from "@/components/SeverityBullet.vue"
 import IoTooltip from "@/components/tooltip/IoTooltip.vue"
 import TimeTooltip from "@/components/tooltip/TimeTooltip.vue"
 import useNode from "@/node"
-import { directive as vTippy } from "vue-tippy"
+import { Tippy, directive as vTippy } from "vue-tippy"
 import { HelpService } from "@/services/help-service"
 const helpService = new HelpService()
 const getNodeTypeDescription = helpService.getNodeTypeDescription