4f403df8821180257a6207e9cdf5593a0e94800e
Pierre GIRAUD <pierre.giraud@dalibo.com>
2025-11-24 09:23:03 +0100

    Animate nodes and edges

---
+++ src/components/AnimatedEdge.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+import { ref, watch } from "vue"
+import { animate } from "motion-v"
+
+interface Props {
+  d: string
+  strokeColor: string
+  strokeWidth?: number
+}
+const props = defineProps<Props>()
+
+const path = ref<SVGPathElement | null>(null)
+
+watch(
+  () => props.d,
+  (newVal) => {
+    if (path.value) {
+      animate(
+        path.value,
+        { d: newVal },
+        {
+          duration: 0.2,
+          ease: "easeInOut",
+        },
+      )
+    }
+  },
+)
+</script>
+
+<template>
+  <path
+    ref="path"
+    :d="d"
+    :stroke="strokeColor"
+    fill="none"
+    :stroke-width="strokeWidth"
+  />
+</template>
+
+<style scoped>
+path {
+  stroke-linecap: butt;
+  &.never-executed {
+    stroke-dasharray: 0.5em;
+    stroke-opacity: 0.5;
+  }
+}
+</style>
