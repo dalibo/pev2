8899ca1955ebfa55b30ff16a8bef59de309c4393
Pierre GIRAUD <pierre.giraud@dalibo.com>
2025-11-27 10:56:10 +0100

    Add link to home page (with logo)

--- example/src/layouts/MainLayout.vue
+++ example/src/layouts/MainLayout.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { inject } from "vue"
 import VLink from "../components/VLink.vue"
 
 interface Props {
@@ -6,17 +7,23 @@ interface Props {
 }
 
 const props = defineProps<Props>()
+const currentPath = inject("currentPath")
 </script>
 
 <template>
   <div class="d-flex flex-column vh-100">
-    <div class="navbar container">
-      <div v-if="props?.title" class="text-center ms-auto">
-        {{ props?.title }}
+    <nav class="navbar">
+      <div :class="[currentPath == '/plan' ? 'container-fluid' : 'container']">
+        <VLink class="btn btn-link" to="/">
+          <img src="../assets/logo_pev2.svg" alt="" style="height: 1.5rem" />
+        </VLink>
+        <div v-if="props?.title" class="text-center ms-auto">
+          {{ props?.title }}
+        </div>
+        <VLink class="btn btn-secondary ms-auto" to="/">New Plan</VLink>
+        <VLink class="btn btn-link" to="/about">About</VLink>
       </div>
-      <VLink class="btn btn-secondary ms-auto" to="/">New Plan</VLink>
-      <VLink class="btn btn-link" to="/about">About</VLink>
-    </div>
+    </nav>
     <slot></slot>
   </div>
 </template>
