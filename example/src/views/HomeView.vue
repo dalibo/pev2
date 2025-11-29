69204044d63180257031980b6b05bde2f6f71547
Pierre GIRAUD <pierre.giraud@dalibo.com>
2025-11-29 19:19:39 +0100

    Readd drop zone style change on file drag

--- example/src/views/HomeView.vue
+++ example/src/views/HomeView.vue
@@ -274,3 +274,10 @@ function handleDrop(event: DragEvent) {
     </div>
   </MainLayout>
 </template>
+
+<style scoped>
+.dropzone-over {
+  box-shadow: 0 0 5px rgba(81, 203, 238, 1);
+  border: 1px solid rgba(81, 203, 238, 1);
+}
+</style>
